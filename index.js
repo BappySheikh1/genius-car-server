03f52125a25a80a4b1a4ab2ee8121101b4866a8e
Your Name Main <you@example.com>
2022-11-01 10:50:04 -0700

    api get to mongo db

--- index.js
+++ index.js
@@ -9,19 +9,34 @@ const port =process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
  
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
 
-// user name : geniusDBcar
-// user password : Topl9GHrzrP9iIQj
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.frkx4db.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
     try{
-
-    }
+      const serviceCollection=client.db('geniusCar').collection('services')
+      
+      //get services data to mongodb    
+      app.get('/services',async(req,res)=>{
+        const quary={}
+        const cursor=serviceCollection.find(quary)
+        const services= await cursor.toArray();
+        res.send(services)
+      })
+   
+    //   get products data to mongodb
+    app.get('/products',async (req,res)=>{
+        const quary ={}
+        const cursor=serviceCollection.find(quary)
+        const products=await cursor.toArray();
+        res.send(products)
+    })
+    } 
     finally{
 
     }
