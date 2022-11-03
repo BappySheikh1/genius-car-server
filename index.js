0548ec8bbc47130f038f0b6925d6d396334a3152
Your Name Main <you@example.com>
2022-11-03 10:03:58 -0700

    jwt token added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+const jwt =require('jsonwebtoken');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express()
 const port =process.env.PORT || 5000
@@ -15,6 +16,15 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
     try{
+      
+        // jwt token
+        app.post('/jwt',(req,res)=>{
+            const user =req.body
+            const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+            res.send({token})
+        })
+   
+
         // services collection
       const serviceCollection=client.db('geniusCar').collection('services')
     //   order Collection
