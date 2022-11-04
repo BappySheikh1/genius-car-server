f1642eaa0aee277864762fefc908114522c5ba7e
Your Name Main <you@example.com>
2022-11-04 09:16:08 -0700

    modify added jwt

--- index.js
+++ index.js
@@ -14,13 +14,28 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.frkx4db.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req,res,next){
+    const authHeader = req.headers.authorization
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorization access'})
+    }
+    const token =authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,function(err,decoded){
+        if(err){
+            return  res.status(403).send({message: 'Forbidden access'})
+        }
+        req.decoded =decoded
+        next();
+    })
+}
 async function run(){
     try{
       
         // jwt token
         app.post('/jwt',(req,res)=>{
             const user =req.body
-            const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+            const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'})
             res.send({token})
         })
    
@@ -55,7 +70,11 @@ async function run(){
     })
 
     // Oprder APi
-    app.get('/orders',async (req,res)=>{
+    app.get('/orders',verifyJWT,async (req,res)=>{
+        const decoded =req.decoded
+        if(decoded.email !== req.query.email){
+            res.status(403).send({message : 'Forbidden access'})
+        }
         let quary={}
         if(req?.query?.email){
             quary={
@@ -67,12 +86,12 @@ async function run(){
         res.send(orders)
     })
    
-    app.post('/orders',async(req,res)=>{
+    app.post('/orders',verifyJWT,async(req,res)=>{
         const order =req.body
         const result= await orderCollection.insertOne(order)
         res.send(result);
     })
-    app.patch('/orders/:id', async (req, res) => {
+    app.patch('/orders/:id',verifyJWT, async (req, res) => {
         const id = req.params.id;
         const status = req.body.status
         const query = { _id: ObjectId(id) }
@@ -85,7 +104,7 @@ async function run(){
         res.send(result);
     })
 
-    app.delete('/orders/:id',async(req,res)=>{
+    app.delete('/orders/:id',verifyJWT,async(req,res)=>{
         const id=req.params.id
         const quary={_id: ObjectId(id)}
         const result=await orderCollection.deleteOne(quary)
